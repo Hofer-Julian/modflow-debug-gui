3988a5196ae34b9f1aef57490f42e9db376a60d3
Julian Hofer <julian.hofer@deltares.nl>
2020-01-17 09:52:21 +0100

    Remove matplotlib dependency

--- src/bmi-debug-gui/__main__.py
+++ src/bmi-debug-gui/__main__.py
@@ -1,10 +1,10 @@
 from application import ApplicationWindow
-from matplotlib.backends.qt_compat import QtWidgets
+from PyQt5.QtWidgets import QApplication
 import sys
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = ApplicationWindow()
     window.show()
     sys.exit(app.exec_())
